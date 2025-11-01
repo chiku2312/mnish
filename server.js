a49d0f775035165c90727a8da0b8212108addb3b
siddharth <163295466+chiku2312@users.noreply.github.com>
2025-11-01 13:37:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,3 +76,8 @@ app.get("/rest/getAllUsers", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server running");
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
